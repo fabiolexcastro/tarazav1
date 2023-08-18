44437aee5bd540359d98e27254cafd186b61393b
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-08-18 18:43:33 -0500

    update

--- 03 downscaling temperature variables.R
+++ 03 downscaling temperature variables.R
@@ -19,8 +19,28 @@ srtm <- terra::rast('./tif/srtm/srtm_z9_fill.tif')
 path <- './tif/nasa/cmip6/historical'
 mdls <- dir_ls(path) %>% basename()
 
-
-
+### Function to use ---------------------------------------------------------
+make.down <- function(mdel, varb){
+  
+  mdel <- mdls[1]
+  varb <- 'tasmin'
+  
+  cat('To process: ', mdel, ' ', varb, '\n')
+  fles <- glue('{path}/{mdel}') %>% 
+    dir_ls(., type = 'directory') %>% 
+    grep(varb, ., value = T) %>% 
+    as.character() %>% 
+    dir_ls(., regexp = '.nc$') %>% 
+    as.character()
+  
+  head(fles, 2)
+  
+  
+  
+  
+  
+  
+}
 
 
 
