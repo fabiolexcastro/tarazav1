5f74d77692bcef072ac4063092019a56b6da115c
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-08-25 17:43:56 -0500

    update

---
+++ 04 download hansen.R
@@ -0,0 +1,34 @@
+
+## Download HANSEN dataset
+## Clary Juliana 
+## August 23 th 2023
+
+### Load libraries ----------------------------------------------------------
+require(pacman)
+pacman::p_load(terra, rgdal, sf, fs, tidyverse, rgeos, gtools, stringr, glue, elevatr)
+
+g <- gc(reset = T)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+### Load data -----------------------------------------------------------
+zone <- terra::vect('gpkg/zone-mpios.gpkg')
+plot(zone)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
