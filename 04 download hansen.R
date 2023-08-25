2185d8fd229f862873330506f9e9e72da86e40e2
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-08-25 17:57:24 -0500

    update

--- 04 download hansen.R
+++ 04 download hansen.R
@@ -23,12 +23,14 @@ dir_create(dout)
 
 # To download -------------------------------------------------------------
 hnsn <- gfcanalysis::download_tiles(tiles = tles, output_folder = dout)
+hnsn <- terra::rast('./tif/hansen/raw/Hansen_GFC-2019-v1.7_treecover2000_10N_080W.tif')
+hnsn <- terra::crop(hnsn, zone)
 
+dir_create('./tif/hansen/extent')
+terra::writeRaster(x = hnsn, filename = './tif/hansen/extent/treecover.tif', overwrite = TRUE)
 
-
-
-
-
+plot(hnsn)
+plot(zone, add = T)
 
 
 
