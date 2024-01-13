efc044bed9c595cc7826a4700942cdeacfcf3eb4
fabiolexcastro <fabioalexandercastro@gmail.com>
2024-01-13 09:21:17 -0500

    update

--- 08 check data downscalled.R
+++ 08 check data downscalled.R
@@ -19,7 +19,10 @@ count.files <- function(dir){
   
   cat('Processing ', dir, '\n')
   fles <- dir_ls(dir)
+  fles <- grep('tas', fles, value = T)
   fles <- map(fles, dir_ls, type = 'directory')
+  
+
   
   
   
